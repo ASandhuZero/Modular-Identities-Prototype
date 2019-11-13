b7d5c70093156ca1e94fd96fde70a683a2fe3bd6
asandhuzero <asandhuzero@gmail>
2019-11-12 16:10:15 -0800

    Finished up some of the scaffolding needed for the prototype.

--- MIA/mia.js
+++ MIA/mia.js
@@ -1,103 +1,70 @@
 var stateInformation = {
- "loveToHeroKinship" : "NA",
+ "alricToBeatriceKinship" : "NA",
  "loveToHeroDuty" : "NA",
- "heroToLoveKinship" : "NA",
- "heroToLoveDuty" : "NA",
- "loveToRivalKinship" : "NA",
- "loveToRivalDuty" :"NA",
+ "alricToCedricKinship" : "NA",
+ "alricToCedricDuty" : "NA",
+ "alricToAlricKinship" : "NA",
+ "alricToAlricDuty" :"NA",
  "heroStrength" : "NA",
  "heroIntelligence" : "NA"
 };
 
-
 var setUpInitialState = function(){
-
-	//Give every character a name.
-	/*
-	var heroTrait = {
-		"class" : "trait",
-		"type" : "hero",
-		"first" : "hero",
-		"value" : true
-	};
-	var loveTrait = {
-		"class" : "trait",
-		"type" : "love",
-		"first" : "love",
-		"value" : true
-	};
-	var rivalTrait = {
-		"class" : "trait",
-		"type" : "rival",
-		"first" : "rival",
-		"value" : true
-	};
-
-	//don't need to establish initial kinship values, because they default to 0 in the schema!
-	// But keep it around for testing purposes.
-	var tempKinship = {
-		"class" : "bonds",
-		"type" : "kinship",
-		"first" : "love",
-		"second" : "hero",
-		"value" : 10
-	};
-	
-	//Actually insert this state into CiF's social facts database.
-	cif.set(heroTrait);
-	cif.set(loveTrait);
-	cif.set(rivalTrait);
-	cif.set(tempKinship);
-*/
 	//update our local copies of these variables, and display them.
 	updateLocalStateInformation();
 	displayStateInformation();
 };
 
+var updateTurn = function(){
+	updateLocalStateInformation();
+	displayStateInformation();
+	console.log("update turn has fired.");
+}
+
 var displayStateInformation = function(){
-	document.getElementById("kinshipHeroToLoverNumber").innerHTML = stateInformation.heroToLoveCloseness;
-	document.getElementById("kinshipLoverToHeroNumber").innerHTML = stateInformation.loveToHeroCloseness;
-	document.getElementById("kinshipLoverToRivalNumber").innerHTML = stateInformation.loveToRivalCloseness;
-	document.getElementById("dutyHeroToLoverNumber").innerHTML = stateInformation.heroToLoveDuty;
-	document.getElementById("dutyLoverToHeroNumber").innerHTML = stateInformation.loveToHeroDuty;
-	document.getElementById("dutyLoverToRivalNumber").innerHTML = stateInformation.loveToRivalDuty;
+	document.getElementById("kinshipAlricToBeatriceVal").innerHTML = stateInformation.alricToBeatriceKinship;
+	document.getElementById("kinshipAlricToCedricVal").innerHTML = stateInformation.alricToCedricKinship;
+	document.getElementById("kinshipAlricToAlricVal").innerHTML = stateInformation.alricToAlricKinship;
+	document.getElementById("dutyAlricToBeatriceVal").innerHTML = stateInformation.alricToBeatriceDuty;
+	document.getElementById("dutyAlricToCedricVal").innerHTML = stateInformation.alricToCedricDuty;
+	document.getElementById("dutyAlricToAlricVal").innerHTML = stateInformation.alricToAlricDuty;
 	document.getElementById("heroStrengthNumber").innerHTML = stateInformation.heroStrength;
 	document.getElementById("heroIntelligenceNumber").innerHTML = stateInformation.heroIntelligence;
 };
 
 var updateLocalStateInformation = function(){
 	//First, let's grab the data we'll want to display.
-	var loveToHeroKinshipPred = {
-		"class" : "bonds",
+	var alricToBeatriceKinshipPred = {
+		"class" : "bonds",	
 		"type" : "kinship",
 		"first" : "Alric",
 		"second" : "Beatrice"
 	};
-	var loveToRivalKinshipPred = {
+	var alricToCedricKinshipPred = {
 		"class" : "bonds",
 		"type" : "kinship",
 		"first" : "Alric",
 		"second" : "Cedric"
 	};
-	var heroToLoveKinshipPred = {
+	var alricToAlricKinshipPred = {
 		"class" : "bonds",
 		"type" : "kinship",
 		"first" : "Alric",
 		"second" : "Alric"
 	};
-	var loveToHeroDutyPred = {
+	var alricToBeatricePred = {
 		"class" : "bonds",
 		"type" : "duty",
 		"first" : "Alric",
 		"second" : "Beatrice"
 	};
-	var heroToLoveDutyPred = {
+	var alricToCedricDutyPred = {
 		"class" : "bonds",
 		"type" : "duty",
 		"first" : "Alric",
 		"second" : "Cedric"
 	};
-	var loveToRivalDutyPred = {
+	var alricToAlricDutyPred = {
 		"class" : "bonds",
 		"type" : "duty",
 		"first" : "Alric",
@@ -115,28 +82,28 @@ var updateLocalStateInformation = function(){
 	};
 
 	//Get love to hero kinship.
-	var results = cif.get(loveToHeroKinshipPred);
-	stateInformation.loveToHeroKinship = results[0].value;
+	var results = cif.get(alricToBeatriceKinshipPred);
+	stateInformation.alricToBeatriceKinship = results[0].value;
 
 	//Get love to rival kinship
-	results = cif.get(loveToRivalKinshipPred);
-	stateInformation.loveToRivalKinship= results[0].value;
+	results = cif.get(alricToCedricKinshipPred);
+	stateInformation.alricToAlricKinship= results[0].value;
 
 	//Get hero to Love kinship
-	results = cif.get(heroToLoveKinshipPred);
-	stateInformation.heroToLoveKinship = results[0].value;
+	results = cif.get(alricToAlricKinshipPred);
+	stateInformation.alricToCedricKinship = results[0].value;
 
 	//get love to hero duty
-	results = cif.get(loveToHeroDutyPred);
-	stateInformation.loveToHeroDuty = results[0].value;
+	results = cif.get(alricToBeatricePred);
+	stateInformation.alricToBeatriceDuty = results[0].value;
 
 	//get love to rival duty
-	results = cif.get(loveToRivalDutyPred);
-	stateInformation.loveToRivalDuty = results[0].value;
+	results = cif.get(alricToAlricDutyPred);
+	stateInformation.alricToAlricDuty = results[0].value;
 
 	//get hero to love duty
-	results = cif.get(heroToLoveDutyPred);
-	stateInformation.heroToLoveDuty = results[0].value;
+	results = cif.get(alricToCedricDutyPred);
+	stateInformation.alricToCedricDuty = results[0].value;
 
 	//get hero intelligence.
 	results = cif.get(heroIntelligencePred);
