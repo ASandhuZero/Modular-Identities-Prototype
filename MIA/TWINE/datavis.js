4598282c086c4b94986f8e797422eda120f5f2ac
asandhuzero <12435554+ASandhuZero@users.noreply.github.com>
2020-09-14 02:00:51 -0700

    Got D3 to play nice with the names of my actions.

--- MIA/TWINE/datavis.js
+++ MIA/TWINE/datavis.js
@@ -3,57 +3,56 @@ const width = 800 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
 const chosen_data = d3.json("./data/actions/heroActions/chosenActions.json").then(function(data) {
-    update(data, "blue");
+    update(data, d3.rgb(149, 125, 173));
 });
 const blacksmith_data = d3.json("./data/actions/smithActions/blackSmithActions.json").then(function(data) {
-    update(data, "red");
+    update(data, d3.rgb(254, 200, 216));
 });
-
 
 function update(new_data, color) { 
     const role = new_data.role;
     const svg = d3
-    .select("body")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
-    
+        .select("body")
+        .append("svg")
+        .text(role)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
     // Group used to enforce margin
-    const g = svg.append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    const group = svg.append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
     const bar_height = 50;
     
     // DATA JOIN
-const rect = g
-    .selectAll("rect")
-    .data(new_data.actions)
-    .join(
-        // ENTER
-        // new elements
-        (enter) => {
-            const rect_enter = enter.append("circle")
-            .attr("cx", 50)
-                .attr("cy", 20)
-                .attr("r", 10)
-                .style("fill", color);
-                rect_enter.append("title");
-                return rect_enter;
-        },
-        // UPDATE
-        // update existing elements
-        (update) => update,
-            // EXIT
-            // elements that aren't associated with data
-        (exit) => exit.remove()
+    const circle = group
+        .selectAll("circle")
+        .data(new_data.actions)
+        .join(
+            // ENTER
+            // new elements
+            (enter) => {
+                const node = enter.append('g');
+                node.append("circle")
+                    .attr("cx", 150)
+                    .attr("cy", 20)
+                    .attr("r", 10)
+                    .style("fill", color);
+                node.append('text')
+                    .attr("x", 100)
+                    .attr("y", "0.31em")
+                    .text(function(d) { return d.name; })
+                return node;
+            },
+            // UPDATE
+            // update existing elements
+            (update) => update,
+                // EXIT
+                // elements that aren't associated with data
+            (exit) => exit.remove()
         );
-        
+
     // ENTER + UPDATE
     // both old and new elements
-    rect
-    .attr("cy", (d, i) => i * (bar_height + 5));
+    circle
+        .attr("transform",function(d, i) { return "translate("+[50,100*i]+")";});
     
-    rect.select("title").text((d) => `value: ${d}`);  
 }
-        // Creates sources <svg> element
-
-// Global variable for all data
